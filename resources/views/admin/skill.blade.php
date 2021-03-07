94d09a217eb5ef61be4e6870903c4e9063902e83
mocakbarmaulana <brindel.development@gmail.com>
2021-03-07 20:49:33 +0700

    Add SkillController store

--- resources/views/admin/skill.blade.php
+++ resources/views/admin/skill.blade.php
@@ -1,5 +1,76 @@
 @extends('layouts.admin')
 
+@section('title')
+<title>List Skill</title>
+@endsection
+
 @section('content')
-<h1>Ini Halaman Admin</h1>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">Dashboard</li>
+        <li class="breadcrumb-item active">{{$active}}</li>
+    </ol>
+</nav>
+<div class="mx-3 p-2 bg-light">
+    <div class="row">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-header bg-dark">
+                    <h5>Tambah List</h5>
+                </div>
+                <div class="card-body">
+                    <form action="{{route('skill.store')}}" method="POST">
+                        @csrf
+                        @error('nameSkill')
+                        <span class="text-danger">*{{$message}}</span>
+                        @enderror
+                        <div class="form-group">
+                            <label for="nameSkill">Skill</label>
+                            <input type="text" name="nameSkill" class="form-control" id="nameSkill">
+                        </div>
+                        <button type="submit" class="btn btn-dark">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col ml-5">
+            <div class="card">
+                <div class="card-header bg-dark">
+                    <h5>List Skill</h5>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection

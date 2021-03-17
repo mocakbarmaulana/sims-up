c14320b0ba87acc20bf3884d56aa0774880361c7
mocakbarmaulana <brindel.development@gmail.com>
2021-03-17 12:49:41 +0700

    Fixing learner layout login

--- resources/views/member/login.blade.php
+++ resources/views/member/login.blade.php
@@ -7,16 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+
+    {{-- Css Custom --}}
+    <link rel="stylesheet" href="{{asset('assets/css/style3.css')}}">
 </head>
 
 <body>
 
-    <div class="container d-flex align-items-center justify-content-center" style="height: 100vh">
-        <div class="card py-5" style="width: 25rem">
+    <div class="container d-flex align-items-center justify-content-center flex-column" style="height: 100vh">
+        <div class="card pt-4 px-2 login-card" style="width: 25rem">
             <div class="card-title text-center">
-                <h1><a href="/">Sims Up</a></h1>
+                <h1><a href="/" class="text-dark link-hover">Sign In</a></h1>
             </div>
             <div class="card-body">
                 @if (session('error'))
@@ -27,8 +29,8 @@
                 <form action="{{route('member.login_proses')}}" method="POST">
                     @csrf
                     <div class="form-group">
-                        <input type="email" class="form-control @error('email') is-invalid @enderror"
-                            placeholder="Email" name="email">
+                        <label class="m-0">Email</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email">
                         @error('email')
                         <div class="invalid-feedback" role="alert">
                             <strong>*{{$message}}</strong>
@@ -36,25 +38,31 @@
                         @enderror
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password" name="password">
+                        <label class="mb-0">Password</label>
+                        <input type="password" class="form-control" name="password">
                         @error('password')
                         <div class="invalid-feedback" role="alert">
                             <strong>*{{$message}}</strong>
                         </div>
                         @enderror
                     </div>
-                    <div class="form-group d-flex justify-content-between">
-                        <button type="submit" class="btn btn-primary">Login</button>
-                        <div class="d-flex align-items-center">
+                    <div class="form-group mt-4">
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        {{-- <div class="d-flex align-items-center">
                             <a href="{{route('member.register')}}">Register?</a>
-                        </div>
-                    </div>
-                </form>
+                    </div> --}}
             </div>
         </div>
+        </form>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    <div class="mt-4">
+        <p>Not a User? <a href="#" class="text-dark link-hover"><strong>Sign Up</strong></a></p>
+    </div>
+    </div>
+    </div>
+
+    {{-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
@@ -62,7 +70,8 @@
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
+    </script> --}}
+    <script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
 </body>
 
 </html>
